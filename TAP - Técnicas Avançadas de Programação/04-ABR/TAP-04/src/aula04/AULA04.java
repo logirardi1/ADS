d3d00b088c9e1a55e808c91afaf669e98eab2d29
Lorenzo <41241133+logirardi1@users.noreply.github.com>
2024-04-11 19:43:31 -0300

    10/04 TAP

---
+++ TAP - Técnicas Avançadas de Programação/04-ABR/TAP-04/src/aula04/AULA04.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula04;
+
+import beans.Ponto2D;
+
+/**
+ *
+ * @author aluno
+ */
+public class AULA04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ponto2D ponto01; 
+   }
+    
+}
