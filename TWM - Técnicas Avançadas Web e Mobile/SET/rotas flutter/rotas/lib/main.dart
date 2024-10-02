ed61d703653d422e462fc88eaba9e6739d25f0aa
Lorenzo <lorefaria6@gmail.com>
2024-10-02 12:11:03 -0300

    .

---
+++ TWM - Técnicas Avançadas Web e Mobile/SET/rotas flutter/rotas/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import '/telas/telaprincipal.dart';
+
+void main() {
+  runApp(const Rotas());
+}
---
+++ TWM - Técnicas Avançadas Web e Mobile/SET/rotas flutter/rotas/lib/model/Pessoa.dart
@@ -0,0 +1,6 @@
+class Pessoa {
+  String nome = "";
+  String sobrenome = "";
+
+  Pessoa(this.nome, this.sobrenome);
+}
